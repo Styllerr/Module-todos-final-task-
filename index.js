221a667bd0e5a012d57f4af3388f6441b8514f2e
Yevhenii_Chukavov <cork@ukr.net>
2020-11-23 09:22:12 +0200

    some fix

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const config = require('config');
-// const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,72 +11,13 @@ app.use(cors());
 app.use(express.static(path.join(__dirname + '/pub')));
 app.use('/api/tasks', require('./routes/tasks-route') );
 
-// let db;
 const PORT = process.env.PORT || config.get('port');
 const url = process.env.mongoURI || config.get('mongoURI');
-// const dbname = "todos";
-// const client = new MongoClient(url, {
-//     useUnifiedTopology: true
-// });
 
 app.get('/', (req, res) => {
     res.sendFile('index.html')
 });
 
-// app.get('/tasks', (req, res) => {
-//     db.collection('tasks').find().toArray((err, docs) => {
-//         if (err) {
-//             console.error(err);
-//             return sendStatus(500);
-//         }
-//         res.send(docs);
-//     })
-// })
-// app.get('/tasks/:id', (req, res) => {
-//     db.collection('tasks').findOne({ _id: ObjectID(req.params.id) }, (err, docs) => {
-//         if (err) {
-//             console.error(err);
-//             return res.sendStatus(500);
-//         }
-//         res.send(docs);
-
-//     })
-// });
-// app.post('/tasks/addTask', (req, res) => {
-//     db.collection('tasks').insertOne(req.body, err => {
-//         if (err) {
-//             console.error(err);
-//             return res.sendStatus(500);
-//         }
-//         res.sendStatus(200);
-//     })
-// });
-// app.put('/tasks/:id', (req, res) => {
-//     db.collection('tasks').updateOne({ _id: ObjectID(req.params.id) }, {
-//         $set: {
-//             serviceType: req.body.serviceType,
-//             task: req.body.task,
-//             location: req.body.location,
-//             description: req.body.description
-//         }
-//     }, err => {
-//         if (err) {
-//             console.error(err);
-//             return res.sendStatus(500);
-//         }
-//         res.sendStatus(200);
-//     })
-// });
-
-// app.delete('/tasks/:id', (req, res) => {
-//     db.collection('tasks').deleteOne({ _id: ObjectID(req.params.id) }, err => {
-//         if (err) {
-//             console.error(err);
-//             return res.sendStatus(500);
-//         }
-//         res.sendStatus(200);
-//     })
-// });
 async function start() {
     try {
         await mongoose.connect(url, {
--- routes/tasks-route.js
+++ routes/tasks-route.js
@@ -4,18 +4,18 @@ const router = Router();
 
 router.get('/', async (req, res) => {
     try {
-        const tasks = await Task.find()
-        res.json(tasks)
+        const tasks = await Task.find();
+        res.json(tasks);
     } catch (error) {
-        res.status(500).json({ message: 'Error server' })
+        res.status(500).json({ message: 'Error server' });
     }
 })
 router.get('/:id', async (req, res) => {
     try {
         const tasks = await Task.findById(req.params.id);
-        res.json(tasks)
+        res.json(tasks);
     } catch (error) {
-        res.status(500).json({ message: 'Error server' })
+        res.status(500).json({ message: 'Error server' });
     }
 })
 
@@ -24,29 +24,29 @@ router.post('/addTask', async (req, res) => {
         const { data, serviceType, task, location, description } = req.body;
         const note = new Task({ data, serviceType, task, location, description });
         await note.save();
-        res.status(201).json({ massage: 'Task created' })
+        res.status(201).json({ massage: 'Task created' });
     } catch (error) {
-        res.status(500).json({ message: 'Error server' })
+        res.status(500).json({ message: 'Error server' });
     }
 })
 
 router.put('/:id', async (req, res) => {
     try {
         const { serviceType, task, location, description } = req.body;
-        const note = { serviceType, task, location, description }
+        const note = { serviceType, task, location, description };
         await Task.findByIdAndUpdate(req.params.id, { $set: note });
-        res.status(200).json({ massage: 'Task updated' })
+        res.status(200).json({ massage: 'Task updated' });
     } catch (error) {
-        res.status(500).json({ message: 'Error server' })
+        res.status(500).json({ message: 'Error server' });
     }
 })
 
 router.delete('/:id', async (req, res) => {
     try {
-        await Task.findByIdAndDelete(req.params.id)
-        res.status(200).json({ massage: 'Task deleted' })
+        await Task.findByIdAndDelete(req.params.id);
+        res.status(200).json({ massage: 'Task deleted' });
     } catch (error) {
-        res.status(500).json({ message: 'Error server' })
+        res.status(500).json({ message: 'Error server' });
     }
 })
 
